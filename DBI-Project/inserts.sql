ba317880b39ef4940d65d47e56f76e9f796acdbe
pedrohsxavier <pedro.hsxavier@outlook.com>
2019-05-27 00:35:15 -0300

    inserts

---
+++ DBI-Project/inserts.sql
@@ -0,0 +1,271 @@
+/*
+IFPB - SISTEMAS PARA INTERNET
+Banco de Dados I - NILTON FREIRE
+Projeto de Banco de Dados
+Alunos:	Pedro Henrique de Sales Xavier
+*/
+
+-- Inserindo pessoas
+
+INSERT INTO pessoa (matricula_pessoa, nome, cpf, sexo, rua, bairro, cidade, cep, email, celular)
+VALUES (5541, 'João Pedro Albuquerque de Barros', '05465005440', 'M', 'Av. Generino Maciel', 'Jaguaribe', 'João Pessoa', '58015700', 'joaopab1337@gmail.com', '988330023')
+
+INSERT INTO pessoa (matricula_pessoa, nome, cpf, sexo, rua, bairro, cidade, cep, celular, telefone)
+VALUES (3584, 'Maria José Ferreira de Andrade', '43662107112', 'F','R. Antônio Alves Bezerra', 'Geisel', 'João Pessoa', '58015500', '988254765', '32548744')
+
+INSERT INTO pessoa (matricula_pessoa, nome, cpf, sexo, rua, bairro, cidade, cep, email, celular, telefone)
+VALUES (6647, 'Dulce Urbano de Andrade Falcão Rodrigues', '49435659217', 'F', 'R. João Florentino de Carvalho', 'Centro', 'Campina Grande', '58016500', 'dulce.urb@gmail.com', '987821454', '33254741')
+
+INSERT INTO pessoa (matricula_pessoa, nome, cpf, sexo, rua, bairro, cidade, cep, email, celular)
+VALUES (1205, 'Nathália Emily Pereira da Silva', '58543744121', 'F', 'R. Siqueira Campos', 'São José', 'Campina Grande', '58400453', 'nath.em@hotmail.com', '987874149')
+
+INSERT INTO pessoa (matricula_pessoa, nome, cpf, sexo, rua, bairro, cidade, cep, email, celular, telefone)
+VALUES (111, 'Pablo Maroja de Lima Limeira', '53829523106', 'M', 'R. Professor Severo Rodrigues', 'Popular', 'Santa Rita', '58301000', 'pablo.pml@gmail.com', '988444466', '32226695')
+
+INSERT INTO pessoa (matricula_pessoa, nome, cpf, sexo, rua, bairro, cidade, cep, email, celular)
+VALUES (687, 'Rhamon Pires Espínola', '82979315109', 'M', 'R. Cidade de Pedras de Fogo', 'Indústrias', 'João Pessoa', '58083552', 'rhamone.pires@hotmail.com', '988353516')
+
+INSERT INTO pessoa (matricula_pessoa, nome, cpf, sexo, rua, bairro, cidade, cep, email, celular, telefone)
+VALUES (5621, 'Andreyeves Diniz Mendes', '60671331213', 'M', 'R. Universitário Carlos Marcelo Pinto', 'Torre', 'João Pessoa', '58040350', 'andreyeves.diniz@gmail.com', '988569852', '32464292')
+
+INSERT INTO pessoa (matricula_pessoa, nome, cpf, sexo, rua, bairro, cidade, cep, email, celular, telefone)
+VALUES (2225, 'Letícia de Oliveira Santos', '96960732166', 'F', 'R. Flor de Íris', 'Mangabeira', 'João Pessoa', '58059744', 'leticia.o.santos@gmail.com', '987078485', '32269894')
+
+INSERT INTO pessoa (matricula_pessoa, nome, cpf, sexo, rua, bairro, cidade, cep, celular)
+VALUES (9657, 'Ivonete Farias de Albuquerque', '45003351453', 'F', 'R. Gercino Farias Leite', 'Sandra Cavalcante', 'Campina Grande', '58410805', '987289222')
+
+INSERT INTO pessoa (matricula_pessoa, nome, cpf, sexo, rua, bairro, cidade, cep, email, celular)
+VALUES (1015, 'José Valdeneis de Barros', '44872786513', 'M', 'R. Angola', 'Indústrias', 'João Pessoa', '58083624', 'jovalbarros@ig.com.br', '988888886')
+
+INSERT INTO pessoa (matricula_pessoa, nome, cpf, sexo, rua, bairro, cidade, cep, email, celular)
+VALUES (7782, 'Anderson Silva dos Santos', '05478005443', 'M', 'R. Galdino de Olinda', 'São José', 'Castelo Branco', '58040800', 'ands.santos@gmail.com', '987858884')
+
+INSERT INTO pessoa (matricula_pessoa, nome, cpf, sexo, rua, bairro, cidade, cep, email, celular)
+VALUES (2314, 'Carlos Henrique de Alencar', '73272786841', 'M', 'R. Lima Penantes', 'Bairro dos Estados', 'João Pessoa', '58945624', 'carlos.henrique@outlook.com', '985364281')
+
+INSERT INTO pessoa (matricula_pessoa, nome, cpf, sexo, rua, bairro, cidade, cep, celular)
+VALUES (5902, 'Luana de Lima Nogueira', '58003351853', 'F', 'Av. Josefa Tavares', 'Jaguaribe', 'João Pessoa', '58901200', '998885457')
+
+INSERT INTO pessoa (matricula_pessoa, nome, cpf, sexo, rua, bairro, cidade, cep,email,celular)
+VALUES (5062, 'Maria das Graças de Sousa', '45007799859', 'F', 'Av. José Ferreira da Silva', 'Bancários', 'João Pessoa', '58951230', 'mg_sousa@hotmail.com', '987412146')
+
+INSERT INTO pessoa (matricula_pessoa, nome, cpf, sexo, rua, bairro, cidade, cep,email,celular)
+VALUES (5881, 'Ronaldo Luís Nazário de Lima', '98003351853', 'M', 'Av. Seringueiras', 'Jardim Planalto', 'João Pessoa', '58901223', 'ronalnza@hotmail.com', '984235476')
+
+INSERT INTO pessoa (matricula_pessoa, nome, cpf, sexo, rua, bairro, cidade, cep,email,celular)
+VALUES (7887, 'Severino de Pontes Alencar', '58493341754', 'M', 'Rua Maximiliano Machado', 'Jaguaribe', 'London', '58907853', 'serverino.pontes@hotmail.com', '935647965')
+
+INSERT INTO pessoa (matricula_pessoa, nome, cpf, sexo, rua, bairro, cidade, cep,email,celular)
+VALUES (9887, 'Damon Graham Devereux Hill', '08103561850', 'M', 'South Hill', 'Valentina', 'London', '12501223', 'hilldh@hotmail.com', '984772126')
+
+INSERT INTO pessoa (matricula_pessoa, nome, cpf, sexo, rua, bairro, cidade, cep,email,celular)
+VALUES (9886, 'Eric Sousa de Araújo', '08103561842', 'M', 'R. Araucárias', 'Barroso', 'Guarabira', '12501223', 'er.sous@hotmail.com', '985762326')
+
+
+-- Inserindo Cursos
+
+INSERT INTO curso (codigo_curso, nome, descricao)
+VALUES (41, 'Eletrônica', 'O curso técnico de Eletrônica tem por objetivo habilitar profissionais para desenvolver circuitos eletrônicos, integrar de sistemas eletrônicos e realizar a manutenção de circuitos e sistemas eletrônicos, seguindo normas técnicas, ambientais, de qualidade, de saúde e segurança no trabalho.')
+
+INSERT INTO curso (codigo_curso, nome, descricao)
+VALUES (22, 'Eletrotécnica', 'Instala, opera e mantém elementos de geração, transmissão e distribuição de energia elétrica. Participa na elaboração e no desenvolvimento de projetos de instalações elétricas e de infraestrutura para sistemas de telecomunicações em edificações.')
+
+INSERT INTO curso (codigo_curso, nome, descricao)
+VALUES (26, 'Informática', 'O curso técnico de Informática tem por objetivo habilitar profissionais para desenvolver programas de computador, lendo e interpretando as especificações técnicas, regras do negócio e paradigmas da lógica e das linguagens de programação, aplicando metodologias e processos e utilizando sistemas operacionais, banco de dados e ambientes integrados de desenvolvimento de sistemas.')
+
+INSERT INTO curso (codigo_curso, nome, descricao)
+VALUES (12, 'Edificações', 'Desenvolve e executa projetos de edificações conforme normas técnicas de segurança e de acordo com legislação específica. Planeja a execução e elabora orçamento de obras. Presta assistência técnica no estudo e desenvolvimento de projetos e pesquisas tecnológicas na área de edificações.')
+
+INSERT INTO curso (codigo_curso, nome, descricao)
+VALUES (30, 'Telecomunicações', 'O curso técnico de Telecomunicações atua desde a fonte de informação até o destino, passando pelo processamento – no transmissor e receptor – à transmissão. Assim, este profissional atua planejando, implantando e operando sistemas de telecomunicações.')
+
+
+-- Inserindo Professores
+
+INSERT INTO professor (matricula_professor, data_admissao, salario)
+VALUES (1015, '2005-06-20', 10000)
+
+INSERT INTO professor (matricula_professor, data_admissao, salario)
+VALUES (9657, '2007-09-18', 5000)
+
+INSERT INTO professor (matricula_professor, data_admissao, salario)
+VALUES (6647, '2012-11-04', 5500)
+
+INSERT INTO professor (matricula_professor, data_admissao, salario)
+VALUES (111, '2010-07-28', 6000)
+
+INSERT INTO professor (matricula_professor, data_admissao, salario)
+VALUES (687, '2015-05-22', 5000)
+
+INSERT INTO professor (matricula_professor, data_admissao, salario)
+VALUES (1205, '2000-12-12', 8000)
+
+
+-- Inserindo Disciplinas
+
+INSERT INTO disciplina (codigo_disciplina, nome, matricula_professor)
+VALUES ('EB', 'Eletricidade Básica', 1015)
+
+INSERT INTO disciplina (codigo_disciplina, nome, matricula_professor)
+VALUES ('CL', 'Circuitos Lógicos', 9657)
+
+INSERT INTO disciplina (codigo_disciplina, nome, matricula_professor)
+VALUES ('FI', 'Física', 6647)
+
+INSERT INTO disciplina (codigo_disciplina, nome, matricula_professor)
+VALUES ('CA', 'Cálculo', 111)
+
+INSERT INTO disciplina (codigo_disciplina, nome, matricula_professor)
+VALUES ('PR', 'Programação', 1205)
+
+
+-- Inserindo Turmas
+
+INSERT INTO turma (codigo_disciplina, codigo_curso, ano_semestre)
+VALUES ('EB', 41, '2016-1')
+
+INSERT INTO turma (codigo_disciplina, codigo_curso, ano_semestre)
+VALUES ('CL', 22, '2016-1')
+
+INSERT INTO turma (codigo_disciplina, codigo_curso, ano_semestre)
+VALUES ('FI', 26, '2016-1')
+
+INSERT INTO turma (codigo_disciplina, codigo_curso, ano_semestre)
+VALUES ('CA', 12, '2016-1')
+
+INSERT INTO turma (codigo_disciplina, codigo_curso, ano_semestre)
+VALUES ('PR', 30, '2016-1')
+
+
+-- Inserindo Projetos
+
+INSERT INTO projeto (codigo_projeto, titulo, descricao)
+VALUES (500, 'Circuito de Cubo de LED', 'Esse projeto é simples e utiliza apenas dois circuitos integrados, seu circuito eletrônico é de um cubo de LED 3x3x3')
+
+INSERT INTO projeto (codigo_projeto, titulo, descricao)
+VALUES (501, 'Bloqueadores de Múltiplas Frequências', 'Este projeto tem como objetivo o desenvolvimento de bloqueadores de RF de múltiplas freqüências (para atuação em sistemas celulares, GPS, rádio, etc.)')
+
+INSERT INTO projeto (codigo_projeto, titulo, descricao)
+VALUES (502, 'Processamento de Sinais em Segurança da Informação', 'O presente projeto contempla três aplicações de processamento de sinais relacionadas à área de Engenharia de Defesa e em particular à Segurança da Informação.')
+
+INSERT INTO projeto (codigo_projeto, titulo, descricao)
+VALUES (503, 'Rádio Definido por Software', 'O objetivo deste projeto é desenvolver um Rádio Definido por Software (RDS), ou "Software Defined Radio" (SDR), viabilizando o desenvolvimento de soluções para sistemas de telecomunicações sem fio de quarta geração.')
+
+INSERT INTO projeto (codigo_projeto, titulo, descricao)
+VALUES (504, 'Processamento adaptativo de sinais', 'investigar filtros adaptativos com restrições lineares e algoritmos robustos e de baixa complexidade, direciona-se as pesquisas neste projeto para novos aspectos do processamento adaptativo de sinais')
+
+
+-- Inserindo Historicos
+
+INSERT INTO historico (codigo_historico, carga_horaria, situacao, codigo_projeto)
+VALUES (11, 200, 'Finalizado', 500)
+
+INSERT INTO historico (codigo_historico, carga_horaria, situacao, codigo_projeto)
+VALUES (32, 460, 'Em andamento', 501)
+
+INSERT INTO historico (codigo_historico, carga_horaria, situacao, codigo_projeto)
+VALUES (44, 6000, 'Em andamento', 502)
+
+INSERT INTO historico (codigo_historico, carga_horaria, situacao, codigo_projeto)
+VALUES (53, 320, 'Finalizado', 503)
+
+INSERT INTO historico (codigo_historico, carga_horaria, situacao, codigo_projeto)
+VALUES (70, 1200, 'Inativo', 504)
+
+
+-- Inserindo Alunos
+
+INSERT INTO aluno (matricula_aluno, nota_vestibular, codigo_curso)
+VALUES (5541, 840, 12)
+
+INSERT INTO aluno (matricula_aluno, nota_vestibular, codigo_curso)
+VALUES (3584, 700, 12)
+
+INSERT INTO aluno (matricula_aluno, nota_vestibular, codigo_curso)
+VALUES (5621, 580, 41)
+
+INSERT INTO aluno (matricula_aluno, nota_vestibular, codigo_curso)
+VALUES (7782, 840, 41)
+
+INSERT INTO aluno (matricula_aluno, nota_vestibular, codigo_curso)
+VALUES (2314, 630, 26)
+
+INSERT INTO aluno (matricula_aluno, nota_vestibular, codigo_curso)
+VALUES (5902, 840, 30)
+
+
+-- Inserções na tabela Ministra
+
+INSERT INTO ministra (codigo_disciplina, codigo_curso, ano_semestre, matricula_professor)
+VALUES ('EB', 41, '2016-1', 1015)
+
+INSERT INTO ministra (codigo_disciplina, codigo_curso, ano_semestre, matricula_professor)
+VALUES ('CL', 22, '2016-1', 9657)
+
+INSERT INTO ministra (codigo_disciplina, codigo_curso, ano_semestre, matricula_professor)
+VALUES ('PR', 30, '2016-1', 1205)
+
+INSERT INTO ministra (codigo_disciplina, codigo_curso, ano_semestre, matricula_professor)
+VALUES ('FI', 26, '2016-1', 6647)
+
+INSERT INTO ministra (codigo_disciplina, codigo_curso, ano_semestre, matricula_professor)
+VALUES ('CA', 12, '2016-1', 111)
+
+
+-- Inserções na tabela Aluno_turma
+
+INSERT INTO aluno_turma (codigo_disciplina, codigo_curso, ano_semestre, matricula_aluno, codigo_projeto)
+VALUES('EB', 41, '2016-1', 2314, 500)
+
+INSERT INTO aluno_turma (codigo_disciplina, codigo_curso, ano_semestre, matricula_aluno, codigo_projeto)
+VALUES('EB', 41, '2016-1', 3584, 501)
+
+INSERT INTO aluno_turma (codigo_disciplina, codigo_curso, ano_semestre, matricula_aluno, codigo_projeto)
+VALUES('CL', 22, '2016-1', 5541, 500)
+
+INSERT INTO aluno_turma (codigo_disciplina, codigo_curso, ano_semestre, matricula_aluno, codigo_projeto)
+VALUES('CL', 22, '2016-1', 5621, 502)
+
+INSERT INTO aluno_turma (codigo_disciplina, codigo_curso, ano_semestre, matricula_aluno)
+VALUES('CA', 12, '2016-1', 5902)
+
+INSERT INTO aluno_turma (codigo_disciplina, codigo_curso, ano_semestre, matricula_aluno)
+VALUES('FI', 26, '2016-1', 7782)
+
+
+-- Inserções na tabela Monitoria
+
+INSERT INTO monitoria (codigo_disciplina, codigo_curso, ano_semestre, matricula_aluno, matricula_professor)
+VALUES ('EB', 41, '2016-1', 2314, 1015)
+
+INSERT INTO monitoria (codigo_disciplina, codigo_curso, ano_semestre, matricula_aluno, matricula_professor)
+VALUES ('CL', 26, '2016-1', 7782, 6647)
+
+INSERT INTO monitoria (codigo_disciplina, codigo_curso, ano_semestre, matricula_aluno, matricula_professor)
+VALUES ('PR', 30, '2016-1', 5541, 9657)
+
+INSERT INTO monitoria (codigo_disciplina, codigo_curso, ano_semestre, matricula_aluno, matricula_professor)
+VALUES ('CA', 22, '2016-1', 3854, 1205)
+
+INSERT INTO monitoria (codigo_disciplina, codigo_curso, ano_semestre, matricula_aluno, matricula_professor)
+VALUES ('FI', 12, '2016-1', 5621, 687)
+
+-- Inserções na tabela Nota
+
+INSERT INTO nota (codigo_disciplina,codigo_curso, ano_semestre, matricula_aluno, nota)
+VALUES ('EB',41, '2016-1', 2314, 71)
+
+INSERT INTO nota (codigo_disciplina,codigo_curso, ano_semestre, matricula_aluno, nota)
+VALUES ('EB', 41, '2016-1', 3584, 85)
+
+INSERT INTO nota (codigo_disciplina,codigo_curso, ano_semestre, matricula_aluno, nota)
+VALUES ('CL', 22, '2016-1', 5541, 60)
+
+INSERT INTO nota (codigo_disciplina,codigo_curso, ano_semestre, matricula_aluno, nota)
+VALUES ('CL', 22, '2016-1', 5621, 40)
+
+INSERT INTO nota (codigo_disciplina,codigo_curso, ano_semestre, matricula_aluno, nota)
+VALUES('CA', 12, '2016-1', 5902, 100)
+
+INSERT INTO nota (codigo_disciplina,codigo_curso, ano_semestre, matricula_aluno)
+VALUES ('FI', 26, '2016-1', 7782)
