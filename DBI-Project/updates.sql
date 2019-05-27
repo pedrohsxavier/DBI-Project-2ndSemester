d1f38e8a9a87139b018798465f49efaca99d81d4
pedrohsxavier <pedro.hsxavier@outlook.com>
2019-05-27 00:37:39 -0300

    updates

---
+++ DBI-Project/updates.sql
@@ -0,0 +1,24 @@
+/*
+IFPB - SISTEMAS PARA INTERNET
+Banco de Dados I - NILTON FREIRE
+Projeto de Banco de Dados
+Alunos:	Pedro Henrique de Sales Xavier
+*/
+
+-- Atualizações
+
+UPDATE professor
+SET matricula_lider = 1015
+WHERE matricula_professor = 9657
+
+UPDATE professor
+SET salario = salario * 1.10
+WHERE SALARIO < 6000
+
+UPDATE nota
+SET nota = nota + 5
+WHERE nota = 85
+
+UPDATE nota
+SET nota = nota + 10
+WHERE NOTA < 5
